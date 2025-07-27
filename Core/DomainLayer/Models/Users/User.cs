1290bf22f00de4172df64504972b3126b1d6aba6
asalaehab824@gmail.com <asalaehab824@gmail.com>
2025-07-27 22:23:30 +0300

    User Enitity

---
+++ Core/DomainLayer/Models/Users/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.Users
+{
+    public class User:IdentityUser
+    {
+
+
+        public string Role { get; set; } = null!;
+
+
+
+    }
+}
